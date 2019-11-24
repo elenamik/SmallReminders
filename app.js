aab803a21c42adbf7c3d7f7f13961883844e3aed
Elena Mikhaylova <Lena@Mac.local>
2019-11-24 15:54:43 -0500

    start script

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world!!\n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
