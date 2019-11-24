665acc37ef0c005d8beab7eb78203021100306f5
Elena Mikhaylova <Lena@Mac.local>
2019-11-24 16:43:07 -0500

    on another branch

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!! This is Lena\n');
+  res.send('Hello world!! This is Lena, from another branch\n');
 });
 
 app.listen(PORT, HOST);
