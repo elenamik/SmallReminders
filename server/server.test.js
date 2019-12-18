f0068fca1a01b2d0028231fd0a51dbb2ba39132b
Elena Mikhaylova <Lena@Mac.local>
2019-12-18 17:43:35 -0500

    full test suite in CI

--- server/server.test.js
+++ server/server.test.js
@@ -35,111 +35,111 @@ describe('Hello world', () => {
   })
 })
 
-// describe('/GET principles/read/', () => {
-//   it( 'should return a JSON object with all principles for user', async ( ) => {
-//     const res = await request(server).get('/principles/read');
-//     expect(res.body).toEqual(
-//       expect.objectContaining({ 
-//         'success': true 
-//       }))
-//   })
-// })
+describe('/GET principles/read/', () => {
+  it( 'should return a JSON object with all principles for user', async ( ) => {
+    const res = await request(server).get('/principles/read');
+    expect(res.body).toEqual(
+      expect.objectContaining({ 
+        'success': true 
+      }))
+  })
+})
 
-// describe('/POST principles/add/?content', () => {
-//   it( 'should add a principle and return resulting JSON object', async ( ) => {
-//     const res = await request(server).post('/principles/add')
-//     .send({
-//       content: "for testing"
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': true
-//     }))
-//     testElement = res.body.result
-//     log('added test element',testElement._id)
-//   })
+describe('/POST principles/add/?content', () => {
+  it( 'should add a principle and return resulting JSON object', async ( ) => {
+    const res = await request(server).post('/principles/add')
+    .send({
+      content: "for testing"
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': true
+    }))
+    testElement = res.body.result
+    log('added test element',testElement._id)
+  })
 
-//   it( 'should fail if content not provided', async ( ) => {
-//     const res = await request(server).post('/principles/add')
-//     .send({
-//       content: undefined
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': false
-//     }))
-//   })
-// })
+  it( 'should fail if content not provided', async ( ) => {
+    const res = await request(server).post('/principles/add')
+    .send({
+      content: undefined
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': false
+    }))
+  })
+})
 
-// describe('/POST principles/update/?id?content', () => {
-//   it( 'should delete a principle and return resulting JSON object', async ( ) => {
-//     const res = await request(server).post('/principles/update')
-//     .send({
-//       id: testElement._id,
-//       content: 'for testing - update'
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': true
-//     }))
-//     log('updated test element',testElement._id)
+describe('/POST principles/update/?id?content', () => {
+  it( 'should delete a principle and return resulting JSON object', async ( ) => {
+    const res = await request(server).post('/principles/update')
+    .send({
+      id: testElement._id,
+      content: 'for testing - update'
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': true
+    }))
+    log('updated test element',testElement._id)
 
-//   })
+  })
 
-//   it( 'should fail if content is not provided', async ( ) => {
-//     const res = await request(server).post('/principles/update')
-//     .send({
-//       id: testElement._id,
-//       content: undefined
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': false
-//     }))
-//   })
+  it( 'should fail if content is not provided', async ( ) => {
+    const res = await request(server).post('/principles/update')
+    .send({
+      id: testElement._id,
+      content: undefined
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': false
+    }))
+  })
 
-//   it( 'should fail if targetId is not provided', async ( ) => {
-//     const res = await request(server).post('/principles/update')
-//     .send({
-//       id: undefined,
-//       content: 'for testing'
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': false
-//     }))
-//   })
+  it( 'should fail if targetId is not provided', async ( ) => {
+    const res = await request(server).post('/principles/update')
+    .send({
+      id: undefined,
+      content: 'for testing'
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': false
+    }))
+  })
 
-// })
+})
 
-// describe('/POST principles/delete/?id', () => {
-//   it( 'should delete a principle and return resulting JSON object', async ( ) => {
-//     const res = await request(server).post('/principles/delete')
-//     .send({
-//       id: testElement._id
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': true
-//       })
-//     )
-//     log('deleted test element',testElement._id)
+describe('/POST principles/delete/?id', () => {
+  it( 'should delete a principle and return resulting JSON object', async ( ) => {
+    const res = await request(server).post('/principles/delete')
+    .send({
+      id: testElement._id
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': true
+      })
+    )
+    log('deleted test element',testElement._id)
 
-//   })
+  })
 
-//   it( 'should fail if delete id not given', async ( ) =>{
-//     const res = await request(server).post('/principles/delete')
-//     .send({
-//       id: undefined
-//     })
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         'success': false
-//       })
-//     )
-//   }
+  it( 'should fail if delete id not given', async ( ) =>{
+    const res = await request(server).post('/principles/delete')
+    .send({
+      id: undefined
+    })
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        'success': false
+      })
+    )
+  }
     
-//   )
+  )
 
-// })
+})
 
