087310ce7acefeac7fc447b8f44614dd7cc3b33f
Elena Mikhaylova <Lena@Mac.lan>
2020-02-19 16:11:31 -0500

    styling

---
+++ src/client/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer () {
+  return (
+    <div id='footer-container'>
+      <div id='footer-content'>
+        <span>
+        Project by Lena Mikhaylova |
+        </span>
+        <a id='github-link' href='https://github.com/elenamik/SmallReminders'>Check it out on github</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
